e82a57895c6afe10b467711320c96a7d55fe05bd
Areeba Sattar <areebasattar23@gmail.com>
2025-02-24 16:29:59 +0500

    Module loaded from installed package

---
+++ custom/Extension/application/Ext/Include/Custom_Modules.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['cust_Logos'] = 'cust_Logos';
+$beanFiles['cust_Logos'] = 'modules/cust_Logos/cust_Logos.php';
+$moduleList[] = 'cust_Logos';
+
+?>
--- custom/application/Ext/Include/modules.ext.php
+++ custom/application/Ext/Include/modules.ext.php
@@ -3,6 +3,23 @@
 
  
  //WARNING: The contents of this file are auto-generated
+$beanList['cust_Logos'] = 'cust_Logos';
+$beanFiles['cust_Logos'] = 'modules/cust_Logos/cust_Logos.php';
+$moduleList[] = 'cust_Logos';
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+$beanList['new_Brands'] = 'new_Brands';
+$beanFiles['new_Brands'] = 'modules/new_Brands/new_Brands.php';
+$moduleList[] = 'new_Brands';
+$beanList['new_Opportunities_Details'] = 'new_Opportunities_Details';
+$beanFiles['new_Opportunities_Details'] = 'modules/new_Opportunities_Details/new_Opportunities_Details.php';
+$moduleList[] = 'new_Opportunities_Details';
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
 $beanList['new_Brands'] = 'new_Brands';
 $beanFiles['new_Brands'] = 'modules/new_Brands/new_Brands.php';
 $moduleList[] = 'new_Brands';
